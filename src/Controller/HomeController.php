41ce29c64d77a9a9732da38f965346af54504c87
TerrorData1 <148558129+TerrorData1@users.noreply.github.com>
2024-03-22 12:48:10 +0100

    page shop

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RecipeRepository;
 
 class HomeController extends AbstractController
 {
@@ -14,5 +15,18 @@ class HomeController extends AbstractController
     {
         // return new Response('Hello, world!');
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/shop', name: 'shop')]
+    public function shop(RecipeRepository $repo): Response
+    {
+
+        $recipes = $repo->findWithDurationLowerThan(20);
+        return $this->render('shop/index.html.twig', [
+            'recipes' => $recipes
+            // $shop = $repo->findAll();
+            // return $this->render('shop/index.html.twig', [
+            //     'shop' => $shop
+        ]);
     }
 }
