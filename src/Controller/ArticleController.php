f866221970bc51b01f579693dc9b49b8d275bf3a
TerrorData1 <148558129+TerrorData1@users.noreply.github.com>
2024-03-27 10:54:41 +0100

    url sans id

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,14 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-
-
-
 class ArticleController extends AbstractController
 {
-
-
 
     // #[Route('/article', name: 'app_article')]
     // public function index(): Response
@@ -24,14 +18,10 @@ class ArticleController extends AbstractController
     // }
 
 
-
-
-
     // public function index(Request $request): Response
     // {
     //     return $this->render('article/index.html.twig');
     // }
-
 
     // #[Route('/article/{slug}-{id}', name: 'article.index', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
     // public function index(Request $request, string $slug, int $id): Response
@@ -39,21 +29,26 @@ class ArticleController extends AbstractController
     //     return new Response('Ceci est l\'article ' . $slug . ' avec l\'id ' . $id);
     // }
 
-    #[Route('/article/{slug}-{id}', name: 'article.show', methods: ['GET'], requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id, RecipeRepository $repo): Response
+
+    // chemin avec l'id et slug!!
+
+    // #[Route('/article/{slug}-{id}', name: 'article.show', methods: ['GET'], requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    // public function show(Request $request, string $slug, int $id, RecipeRepository $repo): Response
+    // {
+    // $recipe = $repo->find($id);
+    // return $this->render('article/index.html.twig', [
+    //     'recipe' => $recipe
+    // ]);
+    // }
+
+    //chemin avec slug uniquement et sans l'id!!
+    #[Route('/article/{slug}', name: 'article.show', methods: ['GET'], requirements: ['slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, RecipeRepository $repo): Response
     {
 
-        // $recipe = new Recipe();
-
-        $recipe = $repo->find($id);
-
-
+        $recipe = $repo->findOneBy(['slug' => $slug]);
         return $this->render('article/index.html.twig', [
             'recipe' => $recipe
         ]);
-        // dd($slug, $id);
-        //     // return $this->render('article/index.html.twig', [
-        //     //     'controller_name' => 'ArticleController',
-        //     // ]);
     }
 }
